1d374dbe9a7be27cfb2269a3804d5d44ec0754e4
gszzzzzz <cgs.zx6@gmail.com>
2026-09-18 04:49:25 +0900

    style(lexer): expand dense test case literals

--- internal/lexer/template_test.go
+++ internal/lexer/template_test.go
@@ -12,16 +12,141 @@ func TestQuotedTemplates(t *testing.T) {
 		want         []tokenText
 	}{
 		{"empty", `""`, []tokenText{{QuoteOpen, `"`}, {QuoteClose, `"`}}},
-		{"literal", "\"# // /* \uFEFF 😀\"", []tokenText{{QuoteOpen, `"`}, {TemplateText, "# // /* \uFEFF 😀"}, {QuoteClose, `"`}}},
-		{"escapes", `"\n\r\t\"\\\u03A9\U0001f600"`, []tokenText{{QuoteOpen, `"`}, {TemplateText, `\n\r\t\"\\\u03A9\U0001f600`}, {QuoteClose, `"`}}},
-		{"escaped introducers", `"$${x} %%{if} $$${x} %%%{if}"`, []tokenText{{QuoteOpen, `"`}, {TemplateText, `$${x} %%{if} $$${x} %%%{if}`}, {QuoteClose, `"`}}},
-		{"interpolation", `"a${~ x ~}b"`, []tokenText{{QuoteOpen, `"`}, {TemplateText, "a"}, {InterpolationOpen, "${"}, {StripMarker, "~"}, {Whitespace, " "}, {Identifier, "x"}, {Whitespace, " "}, {StripMarker, "~"}, {TemplateSequenceEnd, "}"}, {TemplateText, "b"}, {QuoteClose, `"`}}},
-		{"nested object", `"${{a={b=1}}}"`, []tokenText{{QuoteOpen, `"`}, {InterpolationOpen, "${"}, {OpenBrace, "{"}, {Identifier, "a"}, {Equal, "="}, {OpenBrace, "{"}, {Identifier, "b"}, {Equal, "="}, {Number, "1"}, {CloseBrace, "}"}, {CloseBrace, "}"}, {TemplateSequenceEnd, "}"}, {QuoteClose, `"`}}},
-		{"nested quoted", `"${"${x}"}"`, []tokenText{{QuoteOpen, `"`}, {InterpolationOpen, "${"}, {QuoteOpen, `"`}, {InterpolationOpen, "${"}, {Identifier, "x"}, {TemplateSequenceEnd, "}"}, {QuoteClose, `"`}, {TemplateSequenceEnd, "}"}, {QuoteClose, `"`}}},
-		{"directives", `"%{~if x}y%{endif~}"`, []tokenText{{QuoteOpen, `"`}, {DirectiveOpen, "%{"}, {StripMarker, "~"}, {Identifier, "if"}, {Whitespace, " "}, {Identifier, "x"}, {TemplateSequenceEnd, "}"}, {TemplateText, "y"}, {DirectiveOpen, "%{"}, {Identifier, "endif"}, {StripMarker, "~"}, {TemplateSequenceEnd, "}"}, {QuoteClose, `"`}}},
-		{"expression comments", "\"${/* } */x# }\r\n}\"", []tokenText{{QuoteOpen, `"`}, {InterpolationOpen, "${"}, {BlockComment, "/* } */"}, {Identifier, "x"}, {LineComment, "# }"}, {Newline, "\r\n"}, {TemplateSequenceEnd, "}"}, {QuoteClose, `"`}}},
-		{"expression BOM", "\"\uFEFF${\uFEFFx}\"", []tokenText{{QuoteOpen, `"`}, {TemplateText, "\uFEFF"}, {InterpolationOpen, "${"}, {BOM, "\uFEFF"}, {Identifier, "x"}, {TemplateSequenceEnd, "}"}, {QuoteClose, `"`}}},
-		{"returns to config", `"x"+2`, []tokenText{{QuoteOpen, `"`}, {TemplateText, "x"}, {QuoteClose, `"`}, {Plus, "+"}, {Number, "2"}}},
+		{
+			"literal",
+			"\"# // /* \uFEFF 😀\"",
+			[]tokenText{
+				{QuoteOpen, `"`},
+				{TemplateText, "# // /* \uFEFF 😀"},
+				{QuoteClose, `"`},
+			},
+		},
+		{
+			"escapes",
+			`"\n\r\t\"\\\u03A9\U0001f600"`,
+			[]tokenText{
+				{QuoteOpen, `"`},
+				{TemplateText, `\n\r\t\"\\\u03A9\U0001f600`},
+				{QuoteClose, `"`},
+			},
+		},
+		{
+			"escaped introducers",
+			`"$${x} %%{if} $$${x} %%%{if}"`,
+			[]tokenText{
+				{QuoteOpen, `"`},
+				{TemplateText, `$${x} %%{if} $$${x} %%%{if}`},
+				{QuoteClose, `"`},
+			},
+		},
+		{
+			"interpolation",
+			`"a${~ x ~}b"`,
+			[]tokenText{
+				{QuoteOpen, `"`},
+				{TemplateText, "a"},
+				{InterpolationOpen, "${"},
+				{StripMarker, "~"},
+				{Whitespace, " "},
+				{Identifier, "x"},
+				{Whitespace, " "},
+				{StripMarker, "~"},
+				{TemplateSequenceEnd, "}"},
+				{TemplateText, "b"},
+				{QuoteClose, `"`},
+			},
+		},
+		{
+			"nested object",
+			`"${{a={b=1}}}"`,
+			[]tokenText{
+				{QuoteOpen, `"`},
+				{InterpolationOpen, "${"},
+				{OpenBrace, "{"},
+				{Identifier, "a"},
+				{Equal, "="},
+				{OpenBrace, "{"},
+				{Identifier, "b"},
+				{Equal, "="},
+				{Number, "1"},
+				{CloseBrace, "}"},
+				{CloseBrace, "}"},
+				{TemplateSequenceEnd, "}"},
+				{QuoteClose, `"`},
+			},
+		},
+		{
+			"nested quoted",
+			`"${"${x}"}"`,
+			[]tokenText{
+				{QuoteOpen, `"`},
+				{InterpolationOpen, "${"},
+				{QuoteOpen, `"`},
+				{InterpolationOpen, "${"},
+				{Identifier, "x"},
+				{TemplateSequenceEnd, "}"},
+				{QuoteClose, `"`},
+				{TemplateSequenceEnd, "}"},
+				{QuoteClose, `"`},
+			},
+		},
+		{
+			"directives",
+			`"%{~if x}y%{endif~}"`,
+			[]tokenText{
+				{QuoteOpen, `"`},
+				{DirectiveOpen, "%{"},
+				{StripMarker, "~"},
+				{Identifier, "if"},
+				{Whitespace, " "},
+				{Identifier, "x"},
+				{TemplateSequenceEnd, "}"},
+				{TemplateText, "y"},
+				{DirectiveOpen, "%{"},
+				{Identifier, "endif"},
+				{StripMarker, "~"},
+				{TemplateSequenceEnd, "}"},
+				{QuoteClose, `"`},
+			},
+		},
+		{
+			"expression comments",
+			"\"${/* } */x# }\r\n}\"",
+			[]tokenText{
+				{QuoteOpen, `"`},
+				{InterpolationOpen, "${"},
+				{BlockComment, "/* } */"},
+				{Identifier, "x"},
+				{LineComment, "# }"},
+				{Newline, "\r\n"},
+				{TemplateSequenceEnd, "}"},
+				{QuoteClose, `"`},
+			},
+		},
+		{
+			"expression BOM",
+			"\"\uFEFF${\uFEFFx}\"",
+			[]tokenText{
+				{QuoteOpen, `"`},
+				{TemplateText, "\uFEFF"},
+				{InterpolationOpen, "${"},
+				{BOM, "\uFEFF"},
+				{Identifier, "x"},
+				{TemplateSequenceEnd, "}"},
+				{QuoteClose, `"`},
+			},
+		},
+		{
+			"returns to config",
+			`"x"+2`,
+			[]tokenText{
+				{QuoteOpen, `"`},
+				{TemplateText, "x"},
+				{QuoteClose, `"`},
+				{Plus, "+"},
+				{Number, "2"},
+			},
+		},
 	} {
 		t.Run(test.name, func(t *testing.T) { assertTokens(t, test.source, test.want) })
 	}
@@ -33,7 +158,11 @@ func TestQuotedTemplateErrors(t *testing.T) {
 		kinds        []DiagnosticKind
 	}{
 		{"unclosed quote", `"abc`, []DiagnosticKind{UnterminatedQuotedTemplate}},
-		{"unclosed sequence", `"${x`, []DiagnosticKind{UnterminatedQuotedTemplate, UnterminatedTemplateSequence}},
+		{
+			"unclosed sequence",
+			`"${x`,
+			[]DiagnosticKind{UnterminatedQuotedTemplate, UnterminatedTemplateSequence},
+		},
 		{"unknown escape", `"\q"`, []DiagnosticKind{InvalidEscape}},
 		{"truncated escape", "\"abc\\", []DiagnosticKind{UnterminatedQuotedTemplate, InvalidEscape}},
 		{"short unicode", `"\u12"`, []DiagnosticKind{InvalidEscape}},
@@ -79,13 +208,38 @@ func TestTemplateDiagnosticSpansAndRecovery(t *testing.T) {
 		source string
 		want   []Diagnostic
 	}{
-		{`"${"`, []Diagnostic{{UnterminatedQuotedTemplate, Span{0, 1}}, {UnterminatedTemplateSequence, Span{1, 3}}, {UnterminatedQuotedTemplate, Span{3, 4}}}},
+		{
+			`"${"`,
+			[]Diagnostic{
+				{UnterminatedQuotedTemplate, Span{0, 1}},
+				{UnterminatedTemplateSequence, Span{1, 3}},
+				{UnterminatedQuotedTemplate, Span{3, 4}},
+			},
+		},
 		{"\"a\r\nb\"x", []Diagnostic{{NewlineInQuotedTemplate, Span{2, 4}}}},
 		{"\"a\rb\"x", []Diagnostic{{NewlineInQuotedTemplate, Span{2, 3}}}},
-		{"\"a\r\r\nb\"x", []Diagnostic{{NewlineInQuotedTemplate, Span{2, 3}}, {NewlineInQuotedTemplate, Span{3, 5}}}},
-		{"\"a\r", []Diagnostic{{UnterminatedQuotedTemplate, Span{0, 1}}, {NewlineInQuotedTemplate, Span{2, 3}}}},
+		{
+			"\"a\r\r\nb\"x",
+			[]Diagnostic{
+				{NewlineInQuotedTemplate, Span{2, 3}},
+				{NewlineInQuotedTemplate, Span{3, 5}},
+			},
+		},
+		{
+			"\"a\r",
+			[]Diagnostic{
+				{UnterminatedQuotedTemplate, Span{0, 1}},
+				{NewlineInQuotedTemplate, Span{2, 3}},
+			},
+		},
 		{`"\uD800"x`, []Diagnostic{{InvalidEscape, Span{1, 7}}}},
-		{"<<-E\n${", []Diagnostic{{UnterminatedHeredoc, Span{0, 3}}, {UnterminatedTemplateSequence, Span{5, 7}}}},
+		{
+			"<<-E\n${",
+			[]Diagnostic{
+				{UnterminatedHeredoc, Span{0, 3}},
+				{UnterminatedTemplateSequence, Span{5, 7}},
+			},
+		},
 	} {
 		t.Run(test.source, func(t *testing.T) {
 			result := Lex([]byte(test.source))
